e408878eb944a505a7aa5bc6a3855c484d96b359
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-25 11:23:26 +0300

    Add SpringInitializr files
    We use several dependencies, such as JPA, H2 drivers, Web,
    (maybe we will make use of HATEOAS), and last, but not least,
    Security

---
+++ src/main/java/com/codecool/bankaccountstarter/BankAccountStarterApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bankaccountstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/bankaccountstarter/BankAccountStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.bankaccountstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
