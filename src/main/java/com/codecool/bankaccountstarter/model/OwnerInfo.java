522df0b0f92f35bdf7f871a6b669c656b666b274
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-25 11:56:27 +0300

    Add OwnerInfo wrapper class
    It occured to me now that an owner may have multiple accounts,
    so there is a One-To-Many relationship between owners and accounts,
    which is stored as a Set<Account> field.

---
+++ src/main/java/com/codecool/bankaccountstarter/model/OwnerInfo.java
@@ -0,0 +1,38 @@
+package com.codecool.bankaccountstarter.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class OwnerInfo {
+
+    // attributes may be extended/more detailed later on
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    private Set<Account> accounts;
+
+
+    // Constructors
+
+    public OwnerInfo() {}
+
+    public OwnerInfo(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.accounts = new HashSet<>();
+    }
+
+    public OwnerInfo(Long id, String name, Set<Account> accounts) {
+        this.id = id;
+        this.name = name;
+        this.accounts = accounts;
+    }
+
+}
