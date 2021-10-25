68dc67cc1583166a88e3a00ad7844365d2e1ccf2
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-25 12:23:58 +0300

    Configure security to allow general access for now
    Specifically we need access to the web interface for H2.

---
+++ src/main/java/com/codecool/bankaccountstarter/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.codecool.bankaccountstarter.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring().requestMatchers(PathRequest.toH2Console());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
