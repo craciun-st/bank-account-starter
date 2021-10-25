331ebadfbaef170e05793bc2774bdc5bf62d4bfa
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-25 12:40:57 +0300

    Add other methods to AccountOperations
    Specifically, we will be getting Accounts by Account.id and Account.code
    (This is because we have set the code to be stored with a
    Unique constraint)
    
    Then we will deposit/withdraw. These have a separate String functioning as
    a payload to be checked. (The scenario envisioned is that one can not
    withdraw/deposit money with a simple curl/Postman request - these need to
    be done at a specific authorized point: ATM, proprietary app,
    brick & mortar bank etc. While beyond the scope of this MVP, they
    can be used in the future to confirm this authorization)
    
    The InsufficientFundsException and UnauthorizedOperationException
    come to mind from the transfer/withdrawal/deposit points above.
    
    Transferring amounts between accounts, however, is something that
    can be done within the API itself, via a programmatic request and should
    not need this verification (as long as transfers are traceable).
    
    Displaying the balance returns a double, not a long.
    The long is stored as a fixed point algebra value,
    (the precise value in the centimes of the currency)
    while some rounding logic (again, something to be
    extended in the future) is needed to convert it to this
    floating point algebra Double.
    (simply dividing by 100 and truncating should be OK for
    now)

--- src/main/java/com/codecool/bankaccountstarter/model/AccountOperations.java
+++ src/main/java/com/codecool/bankaccountstarter/model/AccountOperations.java
@@ -3,6 +3,11 @@ package com.codecool.bankaccountstarter.model;
 import com.codecool.bankaccountstarter.model.dto.AccountDto;
 import com.codecool.bankaccountstarter.model.exception.DuplicateAccountCodeException;
 import com.codecool.bankaccountstarter.model.exception.InsufficientFundsException;
+import com.codecool.bankaccountstarter.model.exception.UnauthorizedOperationException;
+
+import java.util.Optional;
+import java.util.Set;
+
 public interface AccountOperations {
 
     AccountDto createLocalAccount(String accountOwner, String accountPassword);
@@ -13,4 +18,30 @@ public interface AccountOperations {
             String accountPassword
     ) throws DuplicateAccountCodeException;
 
+    Optional<Account> getAccountById(Long id);
+
+    Optional<Account> getAccountByCode(String accountCode);
+
+    void depositAmount(
+            Double amount,
+            Currency amountCurrency,
+            Account account,
+            String confirmationPayload
+    ) throws UnauthorizedOperationException;
+
+    void withdrawAmount(
+            Double amount,
+            Currency amountCurrency,
+            Account account,
+            String verificationPayload
+    ) throws InsufficientFundsException, UnauthorizedOperationException;
+
+    void transferAmount(
+            Double amount,
+            Currency amountCurrency,
+            Account fromAccount,
+            Account toAccount
+    ) throws InsufficientFundsException;
+
+    Double displayBalance(Account account);
 }
