3005f33a969d2c9480896852b32a6dedb4c92bb8
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-26 10:42:12 +0300

    More dummy code in DbInit

--- src/main/java/com/codecool/bankaccountstarter/init/DbInit.java
+++ src/main/java/com/codecool/bankaccountstarter/init/DbInit.java
@@ -2,27 +2,48 @@ package com.codecool.bankaccountstarter.init;
 
 import com.codecool.bankaccountstarter.model.Account;
 import com.codecool.bankaccountstarter.model.OwnerInfo;
+import com.codecool.bankaccountstarter.model.dto.AccountDto;
+import com.codecool.bankaccountstarter.model.dto.AccountMapper;
 import com.codecool.bankaccountstarter.model.security.Credentials;
 import com.codecool.bankaccountstarter.repository.AccountRepository;
 import com.codecool.bankaccountstarter.repository.CredentialsRepository;
 import com.codecool.bankaccountstarter.repository.OwnerInfoRepository;
+import com.codecool.bankaccountstarter.service.AccountService;
 import com.github.javafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionSystemException;
+
+import javax.validation.ValidationException;
+
 
 @Component
 public class DbInit implements CommandLineRunner {
 
+    Logger logger = LoggerFactory.getLogger(DbInit.class);
+
     private OwnerInfoRepository ownerInfoRepo;
     private CredentialsRepository credentialsRepo;
     private AccountRepository accountRepo;
+    private AccountMapper accountMapper;
+    private AccountService accountService;
 
     @Autowired
-    public DbInit(OwnerInfoRepository ownerInfoRepo, CredentialsRepository credentialsRepo, AccountRepository accountRepo) {
+    public DbInit(
+            OwnerInfoRepository ownerInfoRepo,
+            CredentialsRepository credentialsRepo,
+            AccountRepository accountRepo,
+            AccountMapper accountMapper,
+            AccountService accountService
+    ) {
         this.ownerInfoRepo = ownerInfoRepo;
         this.credentialsRepo = credentialsRepo;
         this.accountRepo = accountRepo;
+        this.accountMapper = accountMapper;
+        this.accountService = accountService;
     }
 
     @Override
@@ -37,9 +58,27 @@ public class DbInit implements CommandLineRunner {
         OwnerInfo testOwner = testAccount.getOwnerInfo();
         Credentials testCredentials = testAccount.getCredentials();
 
+        testAccount.setBalanceInCents(12345L);
         ownerInfoRepo.save(testOwner);
         credentialsRepo.save(testCredentials);
         accountRepo.save(testAccount);
+
+
+        AccountDto testAccountDto = accountMapper.accountEntityToDto(testAccount);
+        logger.warn(testAccountDto.toString());
+
+        AccountDto testInterfaceDto = new AccountDto(2L, "", "", null, 123.5);
+        try {
+            testInterfaceDto = accountService.createLocalAccount(
+                    "",
+                    ""
+            );
+        } catch (TransactionSystemException|IllegalArgumentException e) {
+            logger.error("Could not commit object!");
+        } catch (ValidationException e) {
+            logger.error("Object is not valid!");
+        }
+        logger.warn(testInterfaceDto.toString());
     }
 
 }
