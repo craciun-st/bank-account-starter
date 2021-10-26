6404768cc7f6b1361d09110b521b0cbcba54b977
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-26 10:37:48 +0300

    Declare new errors being thrown in AccountOperations

--- src/main/java/com/codecool/bankaccountstarter/model/AccountOperations.java
+++ src/main/java/com/codecool/bankaccountstarter/model/AccountOperations.java
@@ -4,44 +4,44 @@ import com.codecool.bankaccountstarter.model.dto.AccountDto;
 import com.codecool.bankaccountstarter.model.exception.DuplicateAccountCodeException;
 import com.codecool.bankaccountstarter.model.exception.InsufficientFundsException;
 import com.codecool.bankaccountstarter.model.exception.UnauthorizedOperationException;
+import org.springframework.transaction.TransactionSystemException;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface AccountOperations {
 
-    AccountDto createLocalAccount(String accountOwner, String accountPassword);
+    AccountDto createLocalAccount(String accountOwner, String accountPassword) throws TransactionSystemException;
 
     AccountDto createSpecificAccount(
             String accountCode,
             String accountOwner,
             String accountPassword
-    ) throws DuplicateAccountCodeException;
+    ) throws DuplicateAccountCodeException, TransactionSystemException;
 
     Optional<Account> getAccountById(Long id);
 
     Optional<Account> getAccountByCode(String accountCode);
 
     void depositAmount(
-            Double amount,
+            double amount,
             Currency amountCurrency,
             Account account,
             String confirmationPayload
-    ) throws UnauthorizedOperationException;
+    ) throws UnauthorizedOperationException, TransactionSystemException;
 
     void withdrawAmount(
-            Double amount,
+            double amount,
             Currency amountCurrency,
             Account account,
             String verificationPayload
-    ) throws InsufficientFundsException, UnauthorizedOperationException;
+    ) throws InsufficientFundsException, UnauthorizedOperationException, TransactionSystemException;
 
     void transferAmount(
-            Double amount,
+            double amount,
             Currency amountCurrency,
             Account fromAccount,
             Account toAccount
-    ) throws InsufficientFundsException;
+    ) throws InsufficientFundsException, TransactionSystemException;
 
     Double displayBalance(Account account);
 }
