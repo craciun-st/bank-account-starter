f31dff7439dab451c0b205048c6cab71d997b33d
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-26 10:33:59 +0300

    Add a persistence.xml to configure EntityManager
    We need to define entity manager to use @Transactional annotations.
    
    We move most of the application.yaml config related to
    persistence to this XML.
    Its name is "default-db-manager", as per JpaDatabaseConfig

---
+++ src/main/java/com/codecool/bankaccountstarter/config/JpaDatabaseConfig.java
@@ -0,0 +1,34 @@
+package com.codecool.bankaccountstarter.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.EntityManagerHolder;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@Configuration
+public class JpaDatabaseConfig {
+
+
+
+
+    @Bean
+    EntityManagerFactory entityManagerFactory() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default-db-manager");
+        return emf;
+    }
+
+    @Autowired
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+
+}
