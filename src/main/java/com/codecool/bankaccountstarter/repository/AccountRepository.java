6d375407eff8a6247405c1e9279f9e4cf6d9fd50
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-26 10:37:19 +0300

    Update AccountRepository with custom function

--- src/main/java/com/codecool/bankaccountstarter/repository/AccountRepository.java
+++ src/main/java/com/codecool/bankaccountstarter/repository/AccountRepository.java
@@ -3,5 +3,9 @@ package com.codecool.bankaccountstarter.repository;
 import com.codecool.bankaccountstarter.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByCode(String accountCode);
 }
